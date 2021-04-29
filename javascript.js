36d1d9d2ac79bda38c1b35bae127a26e22ca4c9f
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2021-04-29 20:56:40 +0600

    Some adds and js file

---
+++ javascript.js
@@ -0,0 +1,22 @@
+/*let username = "Niyazbek"
+const number = 1
+alert("Hello , " + username + "!")
+alert(number + 1)           // Комментарий однострочный
+/* Многострочный комментарий
+  1
+  2
+  3
+  4
+ */
+/*
+let age = prompt('Сколько тебе лет?', 100);
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+*/
+
+let year = prompt("В каком году был EXPO?");
+if(year == 2017){
+  alert(`Да, ты прав!`)
+}
+else {
+  alert(`Нет, подумай еще!`)
+}

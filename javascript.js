7ec2a6381f204f8b72ddb5cfeed3637883763978
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2021-05-03 20:57:29 +0600

    some adds

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ alert(number + 1)           // Комментарий однострочный
 let age = prompt('Сколько тебе лет?', 100);
 alert(`Тебе ${age} лет!`); // Тебе 100 лет!
 */
-
+/*
 let year = prompt("В каком году был EXPO?");
 if(year == 2017){
   alert(`Да, ты прав!`)
@@ -20,3 +20,31 @@ if(year == 2017){
 else {
   alert(`Нет, подумай еще!`)
 }
+*/
+/*
+let year = prompt('Когда было Expo?', ' ');
+
+let message = (year < 2016) ? 'Рановато!' :
+  (year > 2017) ? 'Поздновато' :
+  (age == 2021) ? 'В этом году нет EXPO' :
+  'ДА , правильно!';
+
+alert( message );
+*/
+/*
+let year = 0;
+while(year != 2017){
+  year = prompt("В каком году был EXPO?", '');
+  if(year == 2017){
+    alert(`Да, ты прав!`);
+  }
+  else{
+    alert(`Попробуй еще раз!`);
+  }
+}*/
+function showMessage(from, text) { // аргументы: from, text
+  alert(from + ': ' + text);
+}
+
+showMessage('Астана', '+15'); // Аня: Привет! (*)
+showMessage('Алматы', '+20'); // Аня: Как дела? (**)

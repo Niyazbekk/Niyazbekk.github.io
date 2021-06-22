29b8a1585b211a1e87f19072beb7890f21bd910e
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2021-06-22 16:59:47 +0600

    converter

---
+++ TengeConverter/index.js
@@ -0,0 +1,19 @@
+
+
+function convert(){
+    let num = +document.getElementById("teng").value
+    let selected = document.querySelector('input[name="input_cur"]:checked').value
+    if (Number.isInteger(num) && num > 0) {
+        if(selected == "Dollar") {
+            document.getElementById("ans").innerHTML = (num * 350) + " $"
+        }
+        if (selected == "EURO") {
+            document.getElementById("ans").innerHTML = (num * 508) + " €"
+        }
+        if (selected == "RUB") {
+            document.getElementById("ans").innerHTML = (num * 6) + " ₽"
+        }
+    } else {
+        alert("Enter the number")
+    }
+}

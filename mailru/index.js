8b97b04ca6024460c372d23eef9345cf5a9bd91a
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2021-06-26 02:39:45 +0600

    buttons

--- mailru/index.js
+++ mailru/index.js
@@ -1,5 +1,5 @@
-document.querySelector('.fa-caret-down').onclick = function() {
+function rotate() {
   document.querySelector('.list').classList.toggle('hide');
   document.querySelector('.fa-caret-down').classList.toggle('trans_form');
 
-};
+}
